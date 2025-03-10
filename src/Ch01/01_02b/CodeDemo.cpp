879a2637ce6f126002c100bfd3afd03e5e35f9f7
Rym36771 <rymabderrahim36@gmail.com>
2025-03-10 11:38:38 +0000

    done

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,17 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream> // c une librairie pour ecrire des textes et les lire 
+
+int main()
+{
+    std::cout << "Hi There!" << std::endl ;
+
+    std::cout << std::endl << std::endl ;
+    return 0; //dima main toufa b return 0
+
+}
+
+
+
